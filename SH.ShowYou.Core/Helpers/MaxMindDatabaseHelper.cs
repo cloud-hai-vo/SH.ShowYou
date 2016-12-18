730cdef7f4c1e852af53de86083552593f80afca
ocrenaka <ocrenaka@gmail.com>
2016-12-18 09:29:12 +0700

    Fixed missing key in Binary database

--- SH.ShowYou.Core/Helpers/MaxMindDatabaseHelper.cs
+++ SH.ShowYou.Core/Helpers/MaxMindDatabaseHelper.cs
@@ -17,17 +17,45 @@ namespace SH.ShowYou.Core.Helpers
         {
             var dic = GeoCityReader.Find<Dictionary<string, object>>(ipAddress);
             var geo = new GeoLiteCityLocationViewModel();
-            geo.LocId = ((Dictionary<string, object>)dic["city"])["geoname_id"].ToString();
-            geo.Country = ((Dictionary<string, object>)dic["country"])["iso_code"].ToString();
-            geo.Region = ((Dictionary<string, object>)dic["continent"])["code"].ToString();
-            geo.City = ((Dictionary<string, object>)((Dictionary<string, object>)dic["city"])["names"])["en"].ToString();
+            geo.LocId = GetValue(dic, "city", "geoname_id");
+            geo.Country = GetValue(dic, "country", "iso_code");
+            geo.Region = GetValue(dic, "continent", "code");
+            geo.City = GetValue(dic, "city", "names", "en");
             geo.PostalCode = string.Empty;
-            geo.Latitude = Convert.ToDouble(((Dictionary<string, object>)dic["location"])["latitude"]);
-            geo.Longitude = Convert.ToDouble(((Dictionary<string, object>)dic["location"])["longitude"]);
+            geo.Latitude = Convert.ToDouble(GetValue(dic, "location", "latitude"));
+            geo.Longitude = Convert.ToDouble(GetValue(dic, "location", "longitude"));
             geo.MetroCode = string.Empty;
             geo.AreaCode = string.Empty;
 
-            return geo;           
+            return geo;
+        }
+
+        private static string GetValue(Dictionary<string, object> geoCity, params string[] keys)
+        {
+            var value = string.Empty;
+            Dictionary<string, object> temp = null;
+            if (geoCity.ContainsKey(keys[0]))
+            {
+                temp = (Dictionary<string, object>)geoCity[keys[0]];
+
+                for (int i = 1, size = keys.Length; i < size; i++)
+                {
+                    var key = keys[i];
+                    if (temp.ContainsKey(key))
+                    {
+                        if (i == size - 1)
+                        {
+                            value = temp[key].ToString();
+                        }
+                        else
+                        {
+                            temp = (Dictionary<string, object>)temp[key];
+                        }
+                    }
+                }
+            }            
+
+            return value;
         }
     }
 }
