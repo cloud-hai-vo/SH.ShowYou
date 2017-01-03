3101498aa860b08e9d0950c161a7e30168b3e6e8
ocrenaka <ocrenaka@gmail.com>
2017-01-03 20:57:33 +0700

    Add ping api

---
+++ SH.ShowYou/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using System.Web.Routing;
+
+namespace SH.ShowYou.Controllers
+{
+    public class PingController : ApiController
+    {
+        [Route("ping")]
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok("pong");
+        }
+    }
+}
